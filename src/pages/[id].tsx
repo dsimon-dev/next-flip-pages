b485d6b7a6173300027fa5103e8433410cdc1015
Dominik Simon <dominik@dsimon.dev>
2023-02-17 11:44:14 +0100

    style

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -3,13 +3,13 @@ import { GetStaticPaths, GetStaticProps } from "next";
 
 export default function Image({ id }: { id: number }) {
   return (
-    <main style={{ padding: "100px" }}>
+    <main style={{ padding: "100px", display: "grid", placeItems: "center" }}>
       <img
         src={`/images/${id}.jpg`}
         style={{
           display: "block",
           width: "50vw",
-          height: "80vh",
+          height: "calc(100vh - 200px)",
           objectFit: "cover",
         }}
       />
