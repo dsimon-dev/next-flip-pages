4b0ac358be43400331dd3e14ce03c4455c3e649b
Dominik Simon <dominik@dsimon.dev>
2023-02-17 11:00:06 +0100

    init

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 98,
+  semi: true,
+  trailingComma: "es5",
+  importOrder: ["^\\.\\./", "^\\./"],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
